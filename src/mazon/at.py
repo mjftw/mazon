c03208020dde240735a15a53212424599ee755f5
Merlin Webster <mjftwebster@gmail.com>
2021-07-25 22:23:13 +0100

    Can link cells with neighbors

--- src/mazon/at.py
+++ src/mazon/at.py
@@ -7,3 +7,15 @@ class At:
 
   def __repr__(self) -> str:
     return f"[{self.row},{self.col}]"
+
+  def north(self) -> 'At':
+    return At(self.row - 1, self.col)
+
+  def south(self) -> 'At':
+    return At(self.row + 1, self.col)
+
+  def east(self) -> 'At':
+    return At(self.row, self.col + 1)
+
+  def west(self) -> 'At':
+    return At(self.row, self.col - 1)
--- src/mazon/grid.py
+++ src/mazon/grid.py
@@ -1,14 +1,47 @@
 
 
+from dataclasses import replace
 from mazon.at import At
 from mazon.cell import Cell, Neighbor
-from typing import Dict, Generator, Iterable
+from typing import Dict, Iterable
 
 class Grid:
   def __init__(self, rows: int, cols: int):
     self.rows: int = rows
     self.cols: int = cols
     self.cells = self._setup_cells(rows, cols)
+
+  def link_north(self, at: At):
+    self._validate_at(at)
+    cell = self.cells[at]
+    if cell.north:
+      north = self.cells[at.north()]
+      self.cells[at] = replace(cell, north=replace(cell.north, is_linked=True))
+      self.cells[at.north()] = replace(north, south=replace(north.south, is_linked=True))
+
+  def link_south(self, at: At):
+    self._validate_at(at)
+    cell = self.cells[at]
+    if cell.south:
+      south = self.cells[at.south()]
+      self.cells[at] = replace(cell, south=replace(cell.south, is_linked=True))
+      self.cells[at.south()] = replace(south, north=replace(south.north, is_linked=True))
+
+  def link_east(self, at: At):
+    self._validate_at(at)
+    cell = self.cells[at]
+    if cell.east:
+      east = self.cells[at.east()]
+      self.cells[at] = replace(cell, east=replace(cell.east, is_linked=True))
+      self.cells[at.east()] = replace(east, west=replace(east.west, is_linked=True))
+
+  def link_west(self, at: At):
+    self._validate_at(at)
+    cell = self.cells[at]
+    if cell.west:
+      west = self.cells[at.west()]
+      self.cells[at] = replace(cell, west=replace(cell.west, is_linked=True))
+      self.cells[at.west()] = replace(west, east=replace(west.east, is_linked=True))
 
   def col_ns(self, col: int) -> Iterable[Cell]:
     if 0 <= col < self.cols:
@@ -45,16 +78,16 @@ class Grid:
     }
 
     for at in cells:
-      north_cell = cells.get(At(at.row - 1, at.col))
+      north_cell = cells.get(at.north())
       north_neighbor = north_cell and Neighbor(north_cell, False)
 
-      south_cell = cells.get(At(at.row + 1, at.col))
+      south_cell = cells.get(at.south())
       south_neighbor = south_cell and Neighbor(south_cell, False)
 
-      east_cell = cells.get(At(at.row, at.col + 1))
+      east_cell = cells.get(at.east())
       east_neighbor = east_cell and Neighbor(east_cell, False)
 
-      west_cell = cells.get(At(at.row, at.col - 1))
+      west_cell = cells.get(at.west())
       west_neighbor = west_cell and Neighbor(west_cell, False)
 
       cells[at] = Cell(
@@ -66,3 +99,6 @@ class Grid:
 
     return cells
 
+  def _validate_at(self, at: At):
+    if at not in self.cells:
+      raise IndexError(f'No cell at {at}')
